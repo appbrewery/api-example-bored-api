8ac295102b2eb1ab3c5fd23aad498ff1882ae53e
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2023-06-26 11:57:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -2419,7 +2419,7 @@ app.get("/actvity/:key", (req, res) => {
   });
 });
 
-const port = 80;
+const port = 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
